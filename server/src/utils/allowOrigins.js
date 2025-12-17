1c096e77fca77d5b05e29ae919616d1582707e47
Muhammad Faisal <digital.1faisal@gmail.com>
2025-12-17 14:12:32 +0500

    feat: Add utility to dynamically determine allowed CORS origins based on environment.

--- server/src/utils/allowOrigins.js
+++ server/src/utils/allowOrigins.js
@@ -1,11 +1,11 @@
-import { IS_PRODUCTION, CORS_ALLOWED_ORIGINS, CLIENT_ALLOWED_ORIGINS, ADMIN_ALLOWED_ORIGINS } from "../constants.js";
+import { IS_PRODUCTION, CORS_ALLOWED_ORIGINS, CLIENT_ALLOWED_ORIGINS, ADMIN_ALLOWED_ORIGINS, NODE_ENV } from "../constants.js";
 
 const allowOrigins = () => {
-    let origins = ['http://localhost:5173', 'http://localhost:5174'];
-    // if (!IS_PRODUCTION)
-    //     origins = CORS_ALLOWED_ORIGINS.split(",");
-    // else
-    //     origins = [...CLIENT_ALLOWED_ORIGINS.split(","), ...ADMIN_ALLOWED_ORIGINS.split(",")];
+    let origins = [];
+    if (NODE_ENV === "development")
+        origins = CORS_ALLOWED_ORIGINS.split(",");
+    else
+        origins = [...CLIENT_ALLOWED_ORIGINS.split(","), ...ADMIN_ALLOWED_ORIGINS.split(",")];
 
     return origins;
 }
