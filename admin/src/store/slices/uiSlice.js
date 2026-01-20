b61aba67e4f99e1eae4ee7940c89f6a0b7bfc2df
Muhammad Faisal <digital.1faisal@gmail.com>
2026-01-20 13:17:23 +0500

    data

--- admin/src/store/slices/uiSlice.js
+++
@@ -1,6 +0,0 @@
-export const createUiSlice = (set, get) => ({
-    isSidebarOpen: false,
-    openSidebar: () => set({ isSidebarOpen: true }),
-    closeSidebar: () => set({ isSidebarOpen: false }),
-    toggleSidebar: () => set((state) => ({ isSidebarOpen: !state.isSidebarOpen })),
-});
--- admin/src/store/utils.js
+++
@@ -1,3 +0,0 @@
-export const getErrorMessage = (error) => {
-    return error.response?.data?.message || error.message || "An unexpected error occurred";
-};
