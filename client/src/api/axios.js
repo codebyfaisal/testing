e3523d4ce4072c1504d73a51e7da5565652dee54
codebyfaisal <digital.1faisal@gmail.com>
2026-08-03 23:12:47 +0500

    feat: initialize axios instance with interceptors for error handling and server status tracking

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import usePortfolioStore from "../store/usePortfolioStore";
 
-export const BASE_API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000/api/v1";
+export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL || "http://localhost:4000/api/v1";
 
 const api = axios.create({
     baseURL: BASE_API_URL,
